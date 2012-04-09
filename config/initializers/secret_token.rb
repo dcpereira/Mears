930f0fb255ab4909d9ebfb5a3cbdac486359eaae
David Pereira <dpereira@i-neda.com>
2012-04-09 10:47:50 +0100

    adding project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mears::Application.config.secret_token = '34ed5033240237fec40f8e850d1b1abf2520c92cf601c0aab34b1f79ce20f65f09ee557f6f5a758abc22324febd8c5d9eb8dd6f743d1e7dd53a9ee26353e1dce'
